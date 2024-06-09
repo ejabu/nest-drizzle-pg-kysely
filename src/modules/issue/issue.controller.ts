8d0edef03ffff451a965a0aacda7b653f57a364a
ejabu <muhammadfahreza@gmail.com>
2024-06-09 12:00:27 +0700

    update

--- src/modules/issue/issue.controller.ts
+++ src/modules/issue/issue.controller.ts
@@ -1,18 +1,33 @@
 import { Controller, Get, Post } from '@nestjs/common';
-import { IssueService } from './issue.service';
 
+import { IssueService } from './issue.service';
 
 @Controller('issues')
 export class IssueController {
   constructor(private service: IssueService) {}
 
+  /**
+   * 6,352 requests / sec
+   */
+  @Get('/with-sequelize')
+  async createIssueWithSequelize() {
+    this.service.createIssueWithSequelize();
+  }
+
+  /**
+   * 4,181 requests / sec
+   */
+  @Get('/with-direct-query')
+  async createIssuesDirectQuery() {
+    this.service.createIssuesDirectQuery();
+  }
+
+  /**
+   * 2,069 requests / sec
+   */
   @Get('/with-drizzle')
   async createIssueWithDrizzle() {
     this.service.createIssueWithDrizzle();
   }
 
-  @Get('/with-sequelize')
-  async createIssueWithSequelize() {
-    this.service.createIssueWithSequelize();
-  }
 }
--- src/modules/issue/issue.service.ts
+++ src/modules/issue/issue.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
-import { poolpool } from '../../utils/Poolpool';
-import UUID from '../../utils/uuid.util';
 import { IssueDrizzle } from '../../database/repositories/issue.drizzle';
 import { IssueRepository } from '../../database/repositories/issue.repository';
+import { poolpool } from '../../utils/Poolpool';
+import UUID from '../../utils/uuid.util';
 
 @Injectable()
 export class IssueService {
@@ -11,6 +11,53 @@ export class IssueService {
     private readonly issueDrizzle: IssueDrizzle,
     private readonly issueRepo: IssueRepository,
   ) {}
+
+  async createIssueWithSequelize() {
+    const issues = [
+      {
+        id: UUID.generate().slice(0, 16),
+        source: 'source-1',
+        sourceId: '123',
+        title: 'Issue 1',
+        issueCategory: 'Category 1',
+        issueSubcategory: 'Subcategory 1',
+        category: 'Category A',
+        confidence: 3,
+        severity: 3,
+      },
+      {
+        id: UUID.generate().slice(0, 16),
+        source: 'source-1',
+        sourceId: '124',
+        title: 'Issue 2',
+        issueCategory: 'Category 2',
+        issueSubcategory: 'Subcategory 2',
+        category: 'Category B',
+        confidence: 4,
+        severity: 4,
+      },
+    ];
+
+    try {
+      const res = await this.issueRepo.bulkUpsert(issues);
+      return res;
+    } catch (error) {
+      console.log('error =>\n', error);
+    }
+  }
+
+  async createIssuesDirectQuery() {
+    const orgId = '10101010-d990-4ac3-80c3-8872be0a1a6c';
+
+    const trx = await poolpool.createTransaction(orgId);
+    try {
+      const res = await this.issueDrizzle.directBulkUpsert(trx);
+      await trx.commit();
+      return res;
+    } catch (error) {
+      await trx.rollback();
+    }
+  }
 
   async createIssueWithDrizzle() {
     const orgId = '10101010-d990-4ac3-80c3-8872be0a1a6c';
@@ -46,41 +93,6 @@ export class IssueService {
       return res;
     } catch (error) {
       await trx.rollback();
-    }
-  }
-
-  async createIssueWithSequelize() {
-    const orgId = '10101010-d990-4ac3-80c3-8872be0a1a6c';
-    const issues = [
-      {
-        id: UUID.generate().slice(0, 16),
-        source: 'source-1',
-        sourceId: '123',
-        title: 'Issue 1',
-        issueCategory: 'Category 1',
-        issueSubcategory: 'Subcategory 1',
-        category: 'Category A',
-        confidence: 3,
-        severity: 3,
-      },
-      {
-        id: UUID.generate().slice(0, 16),
-        source: 'source-1',
-        sourceId: '124',
-        title: 'Issue 2',
-        issueCategory: 'Category 2',
-        issueSubcategory: 'Subcategory 2',
-        category: 'Category B',
-        confidence: 4,
-        severity: 4,
-      },
-    ];
-
-    try {
-      const res = await this.issueRepo.bulkUpsert(issues);
-      return res;
-    } catch (error) {
-      console.log('error =>\n', error);
     }
   }
 }
